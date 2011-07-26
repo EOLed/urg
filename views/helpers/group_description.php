19baa10eafc01a0ba92efecf542a912e745fd5d6
Amos Chan <amos.chan@chapps.org>
2011-07-25 23:57:43 -0400

    Removed unnecessary debugging from GroupDescritpion helper

--- views/helpers/group_description.php
+++ views/helpers/group_description.php
@@ -3,7 +3,6 @@ class GroupDescriptionHelper extends AppHelper {
     var $helpers = array("Html", "Time");
 
     function build($options = array()) {
-        CakeLog::write("debug", "options for Group Description helper: " . Debugger::exportVar($options, 3));
         $this->Html->css("/urg/css/urg.css", null, array("inline"=>false));
         $title = $this->Html->tag("h2", $options["group_name"], array("class"=>"group-title"));
         $content = $this->Html->para("group-description", $options["group_description"]);
